e6429dd6d5cd7e00a544dfe6128dd0f44990c1c7
okidor <killian_the_killer@hotmail.fr>
2018-11-02 23:16:01 +0100

    made all strings lengths work

--- replace.c
+++ replace.c
@@ -1,23 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 4096
+//TODO treat case where expression is between two read buffers
 
-void replace(char buf[BUFFER_SIZE], char *old_string, char *new_string)
+long int replace(char **dest, char buf[BUFFER_SIZE], char *old_string, char *new_string)
 {
-    char *occurence = strstr(buf,old_string);
-    if(occurence != NULL)
+    size_t old_len = strlen(old_string);
+    size_t new_len = strlen(new_string);
+    size_t buf_len = strlen(buf);
+    long int diff_len = new_len - old_len;
+
+    char *occurence = buf;
+    long int total_diff_len = 0;
+    int count = 0;
+
+    while((occurence = strstr(occurence,old_string)) != NULL)
     {
-        if(strlen(old_string) == strlen(new_string))
-        {
-            strncpy(occurence, new_string, strlen(new_string));
-        }
-        else
-        {
-            printf("replacement of different strings not yet supported\n");
-        }
+        count++;
+        occurence += old_len;
     }
+    total_diff_len = count * diff_len;
+    *dest = malloc(sizeof(char) * (buf_len + total_diff_len) + 1);
+    if(*dest == NULL)
+    {
+        printf("malloc failed\n");
+    }
+    (*dest)[sizeof(char) * (buf_len + total_diff_len)] = '\0';
+
+    char *dest_pnt = *dest;
+    char *src_pnt = buf;
+    occurence = buf;
+    while((occurence = strstr(occurence,old_string)) != NULL)
+    {
+        int start_index = occurence - src_pnt;
+        strncpy(dest_pnt, src_pnt, start_index);
+        strncpy(dest_pnt + start_index, new_string, new_len);
+        dest_pnt = dest_pnt + start_index + new_len;
+        src_pnt = src_pnt + start_index + old_len;
+
+        occurence += old_len;
+    }
+    strncpy(dest_pnt, src_pnt, strlen(src_pnt));
+    if((*dest)[sizeof(char) * (buf_len + total_diff_len)] != '\0')
+    {
+        printf("buffer integrity check 2 failed\n");
+    }
+    return total_diff_len;
 }
 
 int main(int argc, char *argv[])
@@ -35,23 +66,36 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    char buf[BUFFER_SIZE];
+    fseek (file, 0, SEEK_END);
+    long int orig_size=ftell (file);
+    rewind(file);
+
+    char buf[BUFFER_SIZE + 1]; buf[BUFFER_SIZE] = '\0';
     long int write_index, read_index = 0;
     size_t nread = 1;
-    while(nread > 0)
+    long int offset = 0;
+    while(orig_size + offset != read_index)
     {
+        memset(buf,0,BUFFER_SIZE);
         nread = fread(buf, 1, BUFFER_SIZE, file);
         if(ferror(file))
         {
             perror("error occurred while reading file: ");
             return 1;
         }
+        if(nread == 0)
+        {
+            break;
+        }
         read_index = ftell(file);
         fseek(file, write_index, SEEK_SET);
 
-        replace(buf,argv[2],argv[3]);
+        char *new_str = NULL;
+        offset += replace(&new_str, buf, argv[2], argv[3]);
 
-        fwrite(buf, 1, nread, file);
+        read_index += offset;
+        fwrite(new_str, 1, nread + offset, file);
+        free(new_str);
         if(ferror(file))
         {
             perror("error occurred while writing file: ");
@@ -60,6 +104,17 @@ int main(int argc, char *argv[])
         write_index = ftell(file);
         fseek(file, read_index, SEEK_SET);
     }
+    if(buf[BUFFER_SIZE] != '\0')
+    {
+        printf("buffer integrity check 1 failed\n");
+    }
     fclose(file);
+    if(offset < 0)
+    {
+        if (truncate(argv[1], orig_size + offset) != 0)
+        {
+            printf("failed to truncate\n");
+        }
+    }
     return 0;
 }
